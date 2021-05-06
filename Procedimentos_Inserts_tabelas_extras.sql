a81041040dace61e1e6fefd041c7a5e54777c697
Aureo Paquete <>
2021-05-06 11:54:32 +0100

    Criação das tabelas FACTS, DIM e MINDIM

--- Procedimentos_Inserts_tabelas_extras.sql
+++ Procedimentos_Inserts_tabelas_extras.sql
@@ -204,3 +204,220 @@ GRANT SELECT ON MIDIA to bdii_1012639;
 
 
 
+
+---------------------------------------------------- Carregamento das ETL ------------------------------------------------------
+
+
+------------------------------------------------- Criação das tabelas ----------------------------------------------------
+CREATE TABLE dim_customers (
+    cust_sk                  NUMBER(6) NOT NULL,
+    cust_id                  NUMBER NOT NULL,
+    cust_first_name          VARCHAR2(20 BYTE)
+        CONSTRAINT ckc_cust_first_name_customer NOT NULL,
+    cust_last_name           VARCHAR2(40 BYTE)
+        CONSTRAINT ckc_cust_last_name_customer NOT NULL,
+    cust_gender              CHAR(1 BYTE),
+    cust_year_of_birth       NUMBER(4) NOT NULL,
+    cust_marital_status      VARCHAR2(20 BYTE),
+    cust_street_address      VARCHAR2(40 BYTE)
+        CONSTRAINT ckc_cust_street_addre_customer NOT NULL,
+    cust_postal_code         VARCHAR2(10 BYTE)
+        CONSTRAINT ckc_cust_postal_code_customer NOT NULL,
+    cust_main_phone_number   VARCHAR2(25 BYTE),
+    cust_income_level        VARCHAR2(30 BYTE),
+    cust_credit_limit        NUMBER,
+    cust_email               VARCHAR2(30 BYTE),
+    cust_total               VARCHAR2(14 BYTE),
+    cust_city                VARCHAR2(12 BYTE) NOT NULL,
+    cust_month_of_birth      NUMBER(2) NOT NULL,
+    cust_birth_date          DATE NOT NULL,
+    cust_day_of_birth        NUMBER(2) NOT NULL,
+    mindim_cities_city_sk    NUMBER(6) NOT NULL
+);
+
+ALTER TABLE dim_customers ADD CONSTRAINT dim_customers_pk PRIMARY KEY ( cust_sk );
+
+ALTER TABLE dim_customers
+    ADD CONSTRAINT dim_customers_mindim_cities_fk FOREIGN KEY ( mindim_cities_city_sk )
+        REFERENCES mindim_cities ( city_sk );
+        
+---------------------------------------------
+
+
+
+CREATE TABLE dim_employee (
+    employee_sk      NUMBER(6) NOT NULL,
+    employee_id      NUMBER(6) NOT NULL,
+    first_name       VARCHAR2(15 BYTE),
+    last_name        VARCHAR2(12 BYTE),
+    email            VARCHAR2(30 BYTE),
+    phone_number     VARCHAR2(20 BYTE),
+    hire_date        DATE,
+    job_id           VARCHAR2(10 BYTE),
+    salary           NUMBER(12),
+    commission_pct   NUMBER(5,3),
+    manager_id       NUMBER(10)
+);
+
+ALTER TABLE dim_employee ADD CONSTRAINT employee_pk PRIMARY KEY ( employee_sk );
+
+-----------------------------------------------
+
+
+
+CREATE TABLE mindim_salary (
+    salary_sk                  NUMBER(6) NOT NULL,
+    sal                        NUMBER(6,2) NOT NULL,
+    com                        NUMBER(6,2) NOT NULL,
+    dim_employee_employee_sk   NUMBER(6) NOT NULL
+);
+
+ALTER TABLE mindim_salary ADD CONSTRAINT mindim_salary_pk PRIMARY KEY ( salary_sk );
+
+ALTER TABLE mindim_salary
+    ADD CONSTRAINT mindim_salary_dim_employee_fk FOREIGN KEY ( dim_employee_employee_sk )
+        REFERENCES dim_employee ( employee_sk );
+        
+------------------------------------------------
+
+
+
+CREATE TABLE mindim_cities (
+    city_sk             NUMBER(6) NOT NULL,
+    city                VARCHAR2(50) NOT NULL,
+    country_name        VARCHAR2(50) NOT NULL,
+    country_subregion   VARCHAR2(50),
+    country_region      VARCHAR2(50),
+    state_province      VARCHAR2(50),
+    gender              CHAR(1)
+);
+
+ALTER TABLE mindim_cities ADD CONSTRAINT dim_cities_pk PRIMARY KEY ( city_sk );
+
+
+        
+-------------------------------------------------
+
+
+
+CREATE TABLE dim_marketing (
+    marketing_sk     NUMBER(6) NOT NULL,
+    marketing_id     NUMBER(6) NOT NULL,
+    name             VARCHAR2(50) NOT NULL,
+    description      VARCHAR2(50) NOT NULL,
+    midia_midia_id   NUMBER(6) NOT NULL,
+    start_date       DATE NOT NULL,
+    end_date         DATE NOT NULL,
+    category         VARCHAR2(50) NOT NULL,
+    cost             NUMBER(8,2) NOT NULL
+);
+
+ALTER TABLE dim_marketing ADD CONSTRAINT dim_marketing_pk PRIMARY KEY ( marketing_sk );
+
+
+
+--------------------------------------------------
+
+CREATE TABLE dim_products (
+    prod_sk                NUMBER(6) NOT NULL,
+    prod_id                NUMBER NOT NULL,
+    prod_name              VARCHAR2(50)
+        CONSTRAINT ckc_prod_name_prod_desc NOT NULL,
+    sub_cat_id             NUMBER(6) NOT NULL,
+    prod_weight_class      NUMBER(2),
+    prod_unit_of_measure   VARCHAR2(20 BYTE),
+    prod_pack_size         VARCHAR2(30 BYTE),
+    prod_status            VARCHAR2(20 BYTE)
+        CONSTRAINT ckc_prod_status_products NOT NULL,
+    prod_list_price        NUMBER(8,2)
+        CONSTRAINT ckc_prod_list_price_products NOT NULL,
+    prod_min_price         NUMBER(8,2)
+        CONSTRAINT ckc_prod_min_price_products NOT NULL,
+    prod_descriptions_id   NUMBER(6) NOT NULL,
+    prod_cost              NUMBER(8,2),
+    prod_desc              VARCHAR2(4000 BYTE)
+        CONSTRAINT ckc_prod_desc_prod_desc NOT NULL,
+    prod_subcategory       VARCHAR2(50 BYTE)
+        CONSTRAINT ckc_prod_subcategory_sub_cate NOT NULL
+);
+
+
+ALTER TABLE dim_products ADD CONSTRAINT dim_products_pk PRIMARY KEY ( prod_sk );
+
+
+
+----------------------------------------------------
+
+
+
+CREATE TABLE dim_date (
+    date_id        NUMBER(6) NOT NULL,
+    day_of_year    NUMBER(2) NOT NULL,
+    day_of_month   NUMBER(2) NOT NULL,
+    day_of_week    NUMBER(1) NOT NULL,
+    month          NUMBER(2) NOT NULL,
+    trimester      NUMBER(1) NOT NULL,
+    year           NUMBER(4) NOT NULL
+);
+
+ALTER TABLE dim_date ADD CONSTRAINT date_pk PRIMARY KEY ( date_id );
+
+
+
+
+---------------------------------------------------------
+
+
+
+
+CREATE TABLE fact_sales (
+    quantity_sold                NUMBER(9) NOT NULL,
+    amount_sold                  NUMBER(9,2) NOT NULL,
+    dim_date_date_id             NUMBER(6) NOT NULL,
+    dim_employee_employee_sk     NUMBER(6) NOT NULL,
+    dim_marketing_marketing_sk   NUMBER(6) NOT NULL,
+    dim_products_prod_sk         NUMBER(6) NOT NULL,
+    dim_customers_cust_sk        NUMBER(6) NOT NULL,
+    mindim_cities_city_sk        NUMBER(6) NOT NULL,
+    mindim_salary_salary_sk      NUMBER(6) NOT NULL
+);
+
+ALTER TABLE fact_sales
+    ADD CONSTRAINT sales_pk PRIMARY KEY ( dim_date_date_id,
+                                          dim_customers_cust_sk,
+                                          dim_products_prod_sk,
+                                          dim_marketing_marketing_sk,
+                                          dim_employee_employee_sk,
+                                          mindim_cities_city_sk,
+                                          mindim_salary_salary_sk );
+
+ALTER TABLE fact_sales
+    ADD CONSTRAINT fact_sales_dim_customers_fk FOREIGN KEY ( dim_customers_cust_sk )
+        REFERENCES dim_customers ( cust_sk );
+
+ALTER TABLE fact_sales
+    ADD CONSTRAINT fact_sales_dim_date_fk FOREIGN KEY ( dim_date_date_id )
+        REFERENCES dim_date ( date_id );
+
+ALTER TABLE fact_sales
+    ADD CONSTRAINT fact_sales_dim_employee_fk FOREIGN KEY ( dim_employee_employee_sk )
+        REFERENCES dim_employee ( employee_sk );
+
+ALTER TABLE fact_sales
+    ADD CONSTRAINT fact_sales_dim_marketing_fk FOREIGN KEY ( dim_marketing_marketing_sk )
+        REFERENCES dim_marketing ( marketing_sk );
+
+ALTER TABLE fact_sales
+    ADD CONSTRAINT fact_sales_dim_products_fk FOREIGN KEY ( dim_products_prod_sk )
+        REFERENCES dim_products ( prod_sk );
+
+ALTER TABLE fact_sales
+    ADD CONSTRAINT fact_sales_mindim_cities_fk FOREIGN KEY ( mindim_cities_city_sk )
+        REFERENCES mindim_cities ( city_sk );
+
+ALTER TABLE fact_sales
+    ADD CONSTRAINT fact_sales_mindim_salary_fk FOREIGN KEY ( mindim_salary_salary_sk )
+        REFERENCES mindim_salary ( salary_sk );
+        
+        
+
